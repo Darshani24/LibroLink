b9b321415a664c124b4b62a4e294232e0de39e9b
unknown <radkumari@std.appsc.sab.ac.lk>
2024-07-12 09:05:20 +0530

    Welcome to LibroLink

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={{alignItems:'center',padding:100}}>
+      <Text style={{
+        paddingTop:100,
+        fontSize:35,
+        textAlign:'center'
+        
+      }} >Welcome to LibroLink!</Text>
+    </View>
+  )
+}
+
+export default index
